0761d432bd1005ff3f795873cac006c4205bd099
ballcaden <ballcaden@gmail.com>
2023-11-15 09:19:10 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double tempC;
+        double tempF;
+        boolean validOutput = false;
+
+        System.out.println("Please enter a temperature in degrees C to be converted to degrees F: ");
+        do {
+            if (scan.hasNextDouble()) {
+                tempC = scan.nextDouble();
+                tempF = (tempC * (9 / 5) + 32);
+                System.out.printf("\nDegrees C: %-5.2f", tempC);
+                System.out.printf("\nDegrees F: %-5.2f", tempF);
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered an invalid temperature value. Please try again");
+            }
+        } while (!validOutput);
     }
 }
